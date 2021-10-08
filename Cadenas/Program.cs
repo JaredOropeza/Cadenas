f11bc04412f3d5f7c0b2d9d53b75336ae33765ca
McGiverSystem <McGiverSystem@MCGIVERSYSTEM>
2021-10-08 17:14:31 -0400

    Agregar archivos de proyecto.

---
+++ Cadenas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cadena;
+
+            Console.WriteLine("Escriba una frase: ");
+            cadena = Console.ReadLine();
+
+            char[] separador = { ' ', ',' };
+            string[] palabras = cadena.Split(separador);
+            int i;
+            for (i = 0; i < palabras.Length; i++) 
+            {
+
+                Console.WriteLine("palabra {0}={1}  ",(i+1), palabras[i]);
+            }
+            Console.ReadKey();
+        }
+        
+        
+
+    }
+			
+
+
+
+	
+    
+}
